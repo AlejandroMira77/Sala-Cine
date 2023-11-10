9510fb2fdd286879b88c9c87cdf58e2c64dc4947
alejandro7 <alejandro.mira7@gmail.com>
2023-11-09 19:11:34 -0500

    convertir un archivo en base64

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  
   title = 'sala-cine';
+  base64: any;
+
+  onInputChanged(event: any) {
+    let file: File = event.target.files[0];
+    let fileReader: FileReader = new FileReader();
+    fileReader.onload = (e) => {
+      this.base64 = fileReader.result
+    }
+    fileReader.readAsDataURL(file);
+  }
 }
